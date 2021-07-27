4f3e2463d7023198bb7899df17a2b58473605e43
谢志浩 <snail915@qq.com>
2021-07-27 11:05:29 +0800

    20210727合并项目

--- HelloWorld/src/package1/helloWorld.java
+++ HelloWorld/src/package1/helloWorld.java
@@ -1,5 +1,5 @@
 /*
- *@Description:
+ *@Description:测试合并项目后的第一个hello world.
  *@author:xiezhh
  *@create:2021-07-27 10:51
  *@Version 1.0
@@ -7,4 +7,7 @@
 package package1;
 
 public class helloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world.");
+    }
 }
